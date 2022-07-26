79da45cd888056a0955b2e56101e5a73c27c3dab
fxparoo <akwuziefunds@gmail.com>
2022-07-26 18:32:01 +0100

    travis.yml modified

---
+++ server.js
@@ -0,0 +1,15 @@
+const FAVORITE_INSTRUCTION = process.env.Name;
+
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function main() {
+  while(true) {
+    console.log("${FAVOURITE_INSTRUCTOR}rocks!");
+    await sleep(5000);
+  }
+}
+
+main();
